e3eb47e17b42df5fa23cb4066f3378e421cdc5b7
annjose <ann.jose@gmail.com>
2014-05-09 16:40:35 -0700

    Portable class library - not working though

---
+++ Xamarin/MyXamSample/MyXam.Shared.Tests/Class1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MyXam.Shared.Tests
+{
+    public class Class1
+    {
+    }
+}
---
+++ Xamarin/MyXamSample/MyXam.Shared/CourseManager.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MyXam.Shared
+{
+    public class CourseManager
+    {
+        IList<Course> courses;
+        int currentIndex = 0;
+
+        public CourseManager()
+        {
+            courses = new List<Course>()
+            {
+                new Course(){ Title = "How to be become an outlier", Description = "Detailed course on how you can become an outlier developer", Image = "flower1"},
+                new Course(){ Title = "iOS Programming Part 1", Description = "Become an iOS programmer easy and fast", Image = "flower1"},
+            };
+        }
+
+        public void MoveToPrev()
+        {
+            if (currentIndex > 0) currentIndex--;
+        }
+        public void MoveToNext()
+        {
+            if (currentIndex < courses.Count - 1) currentIndex++;
+        }
+    }
+}
