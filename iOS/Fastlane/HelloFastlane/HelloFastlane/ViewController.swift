00dd9b404a33e02221ca777a759e9a569111235a
Ann Catherine Jose <ann.jose@gmail.com>
2017-04-01 21:51:51 -0700

    Pleasantly surprised that Fastlane detected the new xcworkspace and started building that automatically. It asked for which scheme to run. So I modified the test lane to give a default scheme to the scan action. Fastlane is pretty cool

--- iOS/Fastlane/HelloFastlane/HelloFastlane/ViewController.swift
+++ iOS/Fastlane/HelloFastlane/HelloFastlane/ViewController.swift
@@ -7,17 +7,61 @@
 //
 
 import UIKit
+import Alamofire
+
+struct HttpBin {
+    let origin: String
+    let url: String
+    
+    init?(json: Any) {
+        guard let jsonDict = json as? NSDictionary else {
+            return nil
+        }
+        guard let origin = jsonDict["origin"] as? String else {
+            return nil
+        }
+        guard let url = jsonDict["url"] as? String else {
+            return nil
+        }
+        
+        self.origin = origin
+        self.url = url
+    }
+}
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        tryAlamofire()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    func tryAlamofire() {
+        Alamofire.request("https://httpbin.org/get").response { (response) in
+            print("Error = \(response.error)")
+            print("Response = \(response.response)")
+            print("Data = \(response.data)")
+            print("Timeline = \(response.timeline)")
+            print("Metrics = \(response.metrics)")
+        }
+        
+        Alamofire.request("https://httpbin.org/get").responseJSON { (response) in
+            print("Response value= \(response.value.debugDescription)")
+            
+            switch response.result {
+            case .success(let value):
+                if let httpBin = HttpBin(json: value) {
+                    print("HttpBin. origin = \(httpBin.origin) Url = \(httpBin.url)")
+                }
+            case .failure(let error):
+                print("Error: \(error)")
+            }
+        }
     }
 
 
